4f54a772e04091afce2b0f0d11c42889f4f4a5e4
Lindelwa <lindelwa.mojake@fnb.co.za>
2022-06-28 19:29:21 +0200

    animation added

--- projects/carousel-accenture/src/lib/carousel-accenture.component.ts
+++ projects/carousel-accenture/src/lib/carousel-accenture.component.ts
@@ -8,6 +8,7 @@ interface product {
   productLink: string;
   productImageAlt: string;
   productDescription: string;
+  order: number;
 }
 @Component({
   selector: 'lib-carousel-accenture',
@@ -18,7 +19,6 @@ export class CarouselAccentureComponent implements OnInit {
   
   activeSlideIndex = 0;
   deviceInfo: any = null;
-  cta = false;
 
   faChevronLeft = faChevronLeft;
   faChevronRight = faChevronRight;
@@ -28,31 +28,36 @@ export class CarouselAccentureComponent implements OnInit {
       productImage: 'https://onlinecms.mtn.co.za/sites/default/files/product-images/apple/iPhone-12-mini-black.png',
       productLink: 'dadsds',
       productImageAlt: 'dsadsad',
-      productDescription: 'iPhone'      
+      productDescription: 'iPhone',
+      order: 1     
     },
     {
       productImage: 'https://onlinecms.mtn.co.za/sites/default/files/product-images/apple/iphone-13-pro-sierra-blue.png',
       productLink: '',
       productImageAlt: '',
-      productDescription: 'iPhone'      
+      productDescription: 'iPhone',
+      order: 1     
     },
     {
       productImage: 'https://onlinecms.mtn.co.za/sites/default/files/product-images/apple/iphone-13-blue-front_0.png',
       productLink: '',
       productImageAlt: '',
-      productDescription: 'iPhone'      
+      productDescription: 'iPhone',
+      order: 3 
     },
     {
       productImage: 'https://onlinecms.mtn.co.za/sites/default/files/product-images/nokia/honor-50-5g-frost-crystal.png',
       productLink: '',
       productImageAlt: '',
-      productDescription: 'Honor'      
+      productDescription: 'Honor',
+      order: 4     
     },
     {
       productImage: 'https://onlinecms.mtn.co.za/sites/default/files/product-images/samsung/galaxy-a53-5g-blue.png',
       productLink: '',
       productImageAlt: '',
-      productDescription: 'Samsung'      
+      productDescription: 'Samsung',
+      order: 5  
     }
     
   ]
@@ -62,18 +67,29 @@ export class CarouselAccentureComponent implements OnInit {
   ngOnInit(): void {
     this.deviceInfo = this.deviceService.deviceType;
     console.log(this.deviceInfo);
+    // for(let i = 0; i < this.products.length; i++){
+    //   if( this.products[i]){
+    //     console.log(this.activeSlideIndex);
+    //   }
+    // }
   }
 
-  prevSlide(): void{
+  prevSlide(){
     console.log('clicked');
     if(this.activeSlideIndex === 0){
       this.activeSlideIndex = this.products.length - 1;
+
     }else{
       this.activeSlideIndex--;
-      console.log(this.activeSlideIndex);
-    }
-  }
-  nextSlide(): void{
+      
+    //   for(let i = 0; i < this.products.length; i++){
+    //     console.log(this.products[i]);
+    //     [this.products[this.activeSlideIndex], this.products[i] ] = [this.products[i], this.products[this.activeSlideIndex]];
+    //     return this.products;
+    // }
+  }}
+
+  nextSlide(){
     console.log('clicked');
     if(this.activeSlideIndex === this.products.length - 1){
       this.activeSlideIndex = 0;
