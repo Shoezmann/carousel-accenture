85e7d5696ee7fd745b12075ada019ab0047ef2d1
Lindelwa <lindelwa.mojake@fnb.co.za>
2022-06-29 09:28:21 +0200

    Busy with module

--- projects/carousel-accenture/src/lib/carousel-accenture.component.ts
+++ projects/carousel-accenture/src/lib/carousel-accenture.component.ts
@@ -1,14 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+import { DeviceDetectorService } from 'ngx-device-detector';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+
+interface product {
+  productImage: string;
+  productLink: string;
+  productImageAlt: string;
+  productDescription: string;
+}
 @Component({
   selector: 'lib-carousel-accenture',
   template: ``,
-  styles:[]
+  styles: []
 })
 export class CarouselAccentureComponent implements OnInit {
 
-  constructor() { }
+  constructor(private deviceService: DeviceDetectorService) { }
 
   ngOnInit(): void {
-
   }
 }
--- projects/carousel-accenture/src/public-api.ts
+++ projects/carousel-accenture/src/public-api.ts
@@ -5,5 +5,4 @@
 export * from './lib/carousel-accenture.service';
 export * from './lib/carousel-accenture.component';
 export * from './lib/carousel-accenture.module';
-
 export * from './lib/item/item.component';
